ff34fc33e60e787b8e981eeb460f4cfa334588f4
Sovisoth <chet.sovisoth@gmail.com>
2023-12-03 01:35:52 +0700

    added more data, fixed displaying issues

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -3,10 +3,14 @@ import Sidebar from "./Sidebar"
 
 const MainContent = () => {
   return (
-    <div className="container-fluid d-flex p-0">
-        <Sidebar/>
-        <ContentList/>
-    </div>
+      <div className="container-fluid p-0 row">
+        <div className="col-lg-2">
+          <Sidebar/>
+        </div>
+        <div className="col-lg-10">
+          <ContentList/>
+        </div>
+      </div>
   )
 }
 
