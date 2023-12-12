df197ef083de6cce6c33a3946a047541ce640be1
ChetSovisoth <chet.sovisoth@gmail.com>
2023-12-12 22:11:57 +0700

    modify footer

--- src/component/General/Footer.jsx
+++ src/component/General/Footer.jsx
@@ -1,25 +1,32 @@
 const Footer = () => {
   return (
-    <div id="footer" className='container-fluid bg-footer m-0 p-0'>
-        <div className='row g-0 text-center text-white'>
-            <div className='col-md-4 '>
-                <p className="lead mt-3">Location</p>
-                <p className='m-0'>Sras Chak, Duan Penh</p>
-                <p>Phnom Penh, Cambodia</p>
+    <div id="footer" className="bg-footer mb-0">
+        <div className="row text-white mx-2 mb-3">
+            <div className="col-4">
+                <h3 className="py-3 fw-bold">KomsanHub</h3>
+                <p style={{maxWidth: "500px"}}>Royal University of Phnom Penh (Campus 1), Russian Federation Blvd (110), Touk Kork, Phnom Penh, Cambodia</p>
             </div>
-            <div className='col-md-4 mt-3'>
-                <p className="lead">Contact Us</p>
-                <p className='m-0'>Chet.sovisoth@gmail.com</p>
-                <p className="m-0">Sak.vicham@gmail.com</p>
-                <p className="mb-3">Thy.chamroeunpiseth@gmail.com</p>
+            <div className="col-4">
+                <h3 className="py-3 fw-bold">Explore</h3>
+                <p className="m-0">Home</p>
+                <p className="m-0">About Us</p>
+                <p className="m-0">Bookmark</p>
             </div>
-            <div className='col-md-4 mt-3'>
-                <p className="lead">Social Media</p>
+            <div className="col-4">
+                <h3 className="py-3 ms-3">Follow Us</h3>
                 <i className="bi bi-facebook mx-3 fs-2"></i>
+                <i className="bi bi-telegram mx-3 fs-2"></i>
                 <i className="bi bi-instagram mx-3 fs-2"></i>
                 <i className="bi bi-twitter-x mx-3 fs-2"></i>
                 <i className="bi bi-github mx-3 fs-2"></i>         
             </div>
+            <div className="pt-3 text-white">
+                <h3>Contact Us</h3>
+                <p>Email: customer_support@komsanhub.com</p>
+                <p>Tel: +855 23 123 456</p>
+            </div>
+            <hr className="border-2"/>
+            <p className="text-center">Copyright &#64; KomsanHub 2023</p>
         </div>
     </div>
   )
