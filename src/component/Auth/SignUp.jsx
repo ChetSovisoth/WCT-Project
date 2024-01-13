f44dc34103af4ef8566e77f7b39e39b6117f6879
Sovisoth Chet <chet.sovisoth@gmail.com>
2024-01-13 14:09:02 +0700

    add signup with google

--- src/component/Auth/SignUp.jsx
+++ src/component/Auth/SignUp.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import {auth, provider} from "../firebase/Firebase.jsx"
+import {signInWithPopup} from "firebase/auth"
+import localStorage from "redux-persist/es/storage"
 const SignUp = () => {
     const [username, setUsername] = useState("");
+    const [value, setValue] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
     const navigate = useNavigate();
+    const handleClick = () => {
+        signInWithPopup(auth,provider).then((data) =>{
+            setValue(data.user.email)
+            localStorage.setItem("email", data.user.email)
+            
+        })
+    }
+    useEffect(() => {
+        setValue(localStorage.getItem("email"))
+        
+    })
 
   const handleSignUp = () => {
     const data = {
@@ -13,36 +28,37 @@ const SignUp = () => {
         password,
     };
     localStorage.setItem(email, JSON.stringify(data));
-    navigate("/");
   };
   return (
     <div className="d-flex flex-column align-items-center justify-content-center mt-5">   
         <div className="w-50 w-xl-25 rounded-5 p-5 bg-white">
             <form onSubmit={handleSignUp}>
-            <h1 className="fs-5 text-center mb-4">Sign Up</h1>
-            <div className="form-floating mb-3">
-                <input type="username" className="form-control" id="floatingSignInUsername" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
-                <label htmlFor="floatingSignInUsername">Username</label>
-            </div>
-            <div className="form-floating mb-3">
-                <input type="email" className="form-control" id="floatingSignInInput" placeholder="name@example.com" onChange={(e) => setEmail(e.target.value)}/>
-                <label htmlFor="floatingSignInInput">Email address</label>
-            </div>
-            <div className="form-floating">
-                <input type="password" className="form-control" id="floatingSignInPassword" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-                <label htmlFor="floatingPassword">Password</label>
-            </div>
-            <div className="d-flex justify-content-end mt-3">
-                <button className="btn btn-secondary mx-1" onClick={handleSignUp}>Sign up</button>
-            </div>
-            <hr />
-            <div className="text-center my-2 d-flex justify-content-center">
-                Already have an account?&nbsp;
-                <Link to="/signin" className="nav-link text-decoration-underline ">Sign In</Link>
-            </div>
+                <h1 className="fs-5 text-center mb-4">Sign Up</h1>
+                <div className="form-floating mb-3">
+                    <input type="username" className="form-control" id="floatingSignInUsername" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
+                    <label htmlFor="floatingSignInUsername">Username</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input type="email" className="form-control" id="floatingSignInInput" placeholder="name@example.com" onChange={(e) => setEmail(e.target.value)}/>
+                    <label htmlFor="floatingSignInInput">Email address</label>
+                </div>
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="floatingSignInPassword" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+                    <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <div className="d-flex justify-content-end mt-3">
+                    <button className="btn btn-secondary mx-1" onClick={handleSignUp}>Sign up</button>
+                </div>
+                <div className="d-flex justify-content-center mt-3">
+                    <button className="border-0 btn btn-primary" onClick={handleClick}>Sign up with google</button>
+                </div>
+                <hr />
+                <div className="text-center my-2 d-flex justify-content-center">
+                    Already have an account?&nbsp;
+                    <Link to="/signin" className="nav-link text-decoration-underline ">Sign In</Link>
+                </div>
             </form>
         </div>
-        
     </div>
   )
 }
--- src/component/firebase/Firebase.jsx
+++ src/component/firebase/Firebase.jsx
@@ -1,23 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAhd79I9jgZazNZOvqPRjpv0BBuq2Y9aPA",
-  authDomain: "auth-72de3.firebaseapp.com",
-  projectId: "auth-72de3",
-  storageBucket: "auth-72de3.appspot.com",
-  messagingSenderId: "1028597272434",
-  appId: "1:1028597272434:web:19320106f3467587a897eb"
+  apiKey: "AIzaSyCYLybQCax_c9lq_4pRdCIcCtbctiN0r3I",
+  authDomain: "login-870e7.firebaseapp.com",
+  projectId: "login-870e7",
+  storageBucket: "login-870e7.appspot.com",
+  messagingSenderId: "224521898004",
+  appId: "1:224521898004:web:dd9c469bc4e176ec23ff01",
+  measurementId: "G-PT1STSE139"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
-export const db = getFirestore(app);
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider();
+export {auth, provider};
